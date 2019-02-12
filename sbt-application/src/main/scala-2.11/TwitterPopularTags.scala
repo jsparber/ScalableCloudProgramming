8218056b4b96fc58c39be8240e6ffd44cf5550e9
Julian Sparber <julian@sparber.net>
2019-02-12 18:54:37 +0100

    add tf-idf and clean up code

--- sbt-application/src/main/scala-2.11/TwitterPopularTags.scala
+++ sbt-application/src/main/scala-2.11/TwitterPopularTags.scala
@@ -1,87 +1,130 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 // scalastyle:off println
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.streaming.twitter._
 import org.apache.spark.SparkConf
+import org.apache.spark.streaming.twitter._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import twitter4j.auth.OAuthAuthorization
+import twitter4j.conf.ConfigurationBuilder
 
-/**
- * Calculates popular hashtags (topics) over sliding 10 and 60 second windows from a Twitter
- * stream. The stream is instantiated with credentials and optionally filters supplied by the
- * command line arguments.
- *
- * Run this on your local machine as
- *
- */
 object TwitterPopularTags {
   def main(args: Array[String]) {
-    val sparkSession = SparkSession
-      .builder
-      .appName("TwitterPopularTags")
-      .getOrCreate()
-
-    // Set logging level if log4j not configured (override by adding log4j.properties to classpath)
+    /* Set logging level if log4j not configured (override by adding log4j.properties to classpath)
     if (!Logger.getRootLogger.getAllAppenders.hasMoreElements) {
-      Logger.getRootLogger.setLevel(Level.WARN)
+      Logger.getRootLogger.setLevel(Level.OFF)
     }
+    */
+
+    /* Overwrite Log level */
+    Logger.getRootLogger.setLevel(Level.OFF)
+
+    val sparkSession = SparkSession
+    .builder
+    .appName("TwitterPopularTags")
+    .getOrCreate()
 
     val sc = sparkSession.sparkContext;
-    // Set the system properties so that Twitter4j library used by twitter stream
-    // can use them to generate OAuth credentials
-    System.setProperty("twitter4j.oauth.consumerKey", sc.getConf.get("spark.twitter.consumerKey"))
-    System.setProperty("twitter4j.oauth.consumerSecret", sc.getConf.get("spark.twitter.consumerSecret"))
-    System.setProperty("twitter4j.oauth.accessToken", sc.getConf.get("spark.twitter.accessToken"))
-    System.setProperty("twitter4j.oauth.accessTokenSecret", sc.getConf.get("spark.twitter.accessTokenSecret"))
+    val cb = new ConfigurationBuilder
 
+    cb.setDebugEnabled(true).setOAuthConsumerKey(sc.getConf.get("spark.twitter.consumerKey"))
+    .setOAuthConsumerSecret(sc.getConf.get("spark.twitter.consumerSecret"))
+    .setOAuthAccessToken(sc.getConf.get("spark.twitter.accessToken"))
+    .setOAuthAccessTokenSecret(sc.getConf.get("spark.twitter.accessTokenSecret"))
+    /*FIXME: Not avaible probabily because of old twitter4j version	.setTweetModeExtended(true) */
+
+    val auth = new OAuthAuthorization(cb.build)
+
+    /* Twitter filter */
     val filters = Array[String]()
     val ssc = new StreamingContext(sc, Seconds(2))
-    val stream = TwitterUtils.createStream(ssc, None, filters)
+    val stream = TwitterUtils.createStream(ssc, Some(auth), filters)
 
-    val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+    val englishTweets = stream.filter(_.getLang() == "en")
+    val data = englishTweets.flatMap(status => {
+        val text = if (status.isRetweeted) {
+          status.getRetweetedStatus.getText
+        } else {
+          status.getText
+        }
+        /* TODO: add filter for all special chars */
+        Array(text).filter(_ >= " ")
+      })
 
-    val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
-                     .map{case (topic, count) => (count, topic)}
-                     .transform(_.sortByKey(false))
+    /* Store every tweet to a text file */
+    data.saveAsTextFiles("hdfs://hadoop:9000/tweets")
 
-    val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
-                     .map{case (topic, count) => (count, topic)}
-                     .transform(_.sortByKey(false))
-
-
-    // Print popular hashtags
-    topCounts60.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
-      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-    })
-
-    topCounts10.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
-      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-    })
-
+    /* Print tweets */
+    data.foreachRDD(rdd => {
+        var top = rdd.take(10)
+        println("\nTweets (%s total):".format(rdd.count()))
+        top.foreach{case (count) => println("%s".format(count))}
+        /* Collect at least on RDD with more then 20 tweets */
+        if (rdd.count > 20) {
+          ssc.stop(false)
+        }
+      })
     ssc.start()
+    /* TODO: use a timeout */
+    /* Wait for stream to terminate */
     ssc.awaitTermination()
+
+    val docs = sc.textFile("hdfs://hadoop:9000/tweets*")
+
+    println("Number of documents to analize: " + docs.count)
+
+    /* Calculate word frequency */
+    val tf = docs.map(doc => {
+        val wordArray = doc.split(" ")
+        /* reduceByKey for array */
+        wordArray.map(word => (word.toLowerCase, 1))
+        .groupBy(_._1)
+        .map(l => (l._1, l._2.map(_._2).reduce(_+_)))
+        .map({case (key, n) => (key, n.toFloat/wordArray.length)})
+      })
+
+    for (a <- tf.collect) {
+      println("TF for each document: " + a)
+    }
+
+    /* Calculate inverse document frequency */
+    val docsSize = docs.count
+    val idf = tf.flatMap(x => x.toList)
+    .map({case (key, n) => (key, 1.0)})
+    .reduceByKey(_ + _)
+    .map({case (key, n) => (key, Math.log10(docsSize.toFloat / n))})
+
+    for (a <- idf.collect) {
+      println("IDF for each Token: " + a)
+    }
+
+    /* FIXME: don't use collect, but probably there isn't a better solution 
+    *  because collect moves all data back to the driver application */
+    val table = idf.collect.toMap
+    /* create vector for each document as a Map */
+    val vector = tf.map(m => m.transform((key, n) => n * table(key)))
+    for (a <- vector.collect) {
+      println("Vector for each document: " + a)
+    }
+
     sparkSession.stop()
   }
 }
